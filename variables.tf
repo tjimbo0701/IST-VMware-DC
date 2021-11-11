bcdf9dc0e6f9464fb633c839771358ebb5092dc2
saurabhkothari <saurabhkothari@rocketmail.com>
2021-11-11 11:15:05 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -51,34 +51,11 @@ variable "vsphere_vm_portgroup" {
 variable "vsphere_vm_cpu" {
   type = number
   description = "the number of vCpus"
-  default = 2
+  default = 1
 }
 
 variable "vsphere_vm_memory" {
   type = number
   description = "the amount of memory in MB"
   default = 2048
-}
-
-variable "vsphere_vm_guest" {
-  type = string
-  description = "the name of the os type "
-  default = "centos8_64Guest"
-}
-
-variable "vsphere_vm_disksize" {
-  type = number
-  description = "the size of the disk in GB"
-  default = 25
-}
-
-variable "timeout" {
-  description = "The timeout, in minutes, to wait for the virtual machine clone to complete."
-  type        = number
-  default     = 30
-}
-
-variable "linked_clone" {
-  description = "Clone this virtual machine from a snapshot. Templates must have a single snapshot only in order to be eligible."
-  default     = false
 }
