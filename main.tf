bca110bbedbe7e994c33d929139e7f92251ca868
saurabhkothari <saurabhkothari@rocketmail.com>
2021-11-11 09:56:34 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,71 @@
+terraform {
+  required_providers {
+    vsphere = {
+      source = "hashicorp/vsphere"
+   
+    }
+  }
+}
+
+provider "vsphere" {
+  user           = var.vsphere_user
+  password       = var.vsphere_password
+  vsphere_server = var.vsphere_server
+
+  # if you have a self-signed cert
+  allow_unverified_ssl = true
+}
+
+
+data "vsphere_datacenter" "dc" {
+  name = var.vsphere_datacenter
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = var.vsphere_datastore
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_resource_pool" "pool" {
+  name          = var.vsphere_resource_pool
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_network" "network" {
+  name          = var.vsphere_vm_portgroup
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_virtual_machine" "template" {
+  name          = var.vsphere_vm_template
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+
+resource "vsphere_virtual_machine" "vm" {
+  name             = var.vsphere_vm_name
+  resource_pool_id = data.vsphere_resource_pool.pool.id
+  datastore_id     = data.vsphere_datastore.datastore.id
+
+  num_cpus = var.vsphere_vm_cpu #2
+  memory   = var.vsphere_vm_memory #1024
+  guest_id = var.vsphere_vm_guest #"other3xLinux64Guest"
+
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
+
+  disk {
+    label = "disk0"
+    size  = var.vsphere_vm_disksize #20
+  }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+    linked_clone  = var.linked_clone
+    timeout       = var.timeout
+
+  }
+
+
+}
