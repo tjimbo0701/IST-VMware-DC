f5014a790c5a8ea9937809e46277a1e967176870
saurabhkothari <saurabhkothari@rocketmail.com>
2022-05-05 20:13:41 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,65 +1,27 @@
-variable "vsphere_user" {
-  type = string
-  description = "the username for vsphere"
-  
-}
-variable "vsphere_password" {
-  type = string
-  description = "The password for vsphere"
-}
-variable "vsphere_server" {
-  type = string
-  description = "the hostname or ip address of your vcenter server"
-  
-}
+variable "vsphere_user" {}
+variable "vsphere_password" {}
+variable "vsphere_server" {}
 
-variable "vsphere_datacenter" {
-  type = string
-  description = "the name of the datacenter"
-  
-}
+variable "vsphere_datacenter" {}
 
-variable "vsphere_datastore" {
-  type = string
-  description = "the name of the datastore"
-  
-}
+variable "vsphere_datastore" {}
 
-variable "vsphere_vm_template" {
-  type = string
-  description = "the name of the vm template"
-  
-}
+variable "vsphere_vm_template" {}
 
-variable "vsphere_vm_name" {
-  type = string
-  description = "the name of the vm"
-}
+variable "vsphere_vm_name" {}
 
-variable "vsphere_folder_name" {
-  type=string
-}
+variable "vsphere_folder_name" {}
 
-variable "vsphere_resource_pool" {
-  type = string
-  description = "the name of the resourcepool for examples: Cluster1/Resources " 
- 
-}
+variable "vsphere_resource_pool" {}
 
-variable "vsphere_vm_portgroup" {
-  type = string
-  description = "the name of the portgroup"
-  
-}
+variable "vsphere_vm_portgroup" {}
 
 variable "vsphere_vm_cpu" {
   type = number
-  description = "the number of vCpus"
   default = 1
 }
 
 variable "vsphere_vm_memory" {
   type = number
-  description = "the amount of memory in MB"
   default = 2048
 }
