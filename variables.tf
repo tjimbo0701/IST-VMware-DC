33e99bfee4cf36cab912b35e4f883ccaa1cab3f1
saurabhkothari <saurabhkothari@rocketmail.com>
2022-05-05 20:14:01 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,26 +1,17 @@
 variable "vsphere_user" {}
 variable "vsphere_password" {}
 variable "vsphere_server" {}
-
 variable "vsphere_datacenter" {}
-
 variable "vsphere_datastore" {}
-
 variable "vsphere_vm_template" {}
-
 variable "vsphere_vm_name" {}
-
 variable "vsphere_folder_name" {}
-
 variable "vsphere_resource_pool" {}
-
 variable "vsphere_vm_portgroup" {}
-
 variable "vsphere_vm_cpu" {
   type = number
   default = 1
 }
-
 variable "vsphere_vm_memory" {
   type = number
   default = 2048
